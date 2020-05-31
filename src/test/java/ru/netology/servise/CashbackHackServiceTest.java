cad8964a4a4542d16d3315643dcf136a0794633e
evgenijerm <erm.evgeniy18@gmail.com>
2020-05-31 19:49:17 +0300

    second commit

---
+++ src/test/java/ru/netology/servise/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.servise;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void AmountLessThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expect = 100;
+        int actual = service.remain(amount);
+        assertEquals(actual, expect);
+    }
+
+    @Test
+    public void AmountMoreThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+        int expect = 800;
+        int actual = service.remain(amount);
+        assertEquals(actual, expect);
+    }
+
+    @Test
+    public void Amount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expect = 1000;
+        int actual = service.remain(amount);
+        assertEquals(actual, expect);
+    }
+}
