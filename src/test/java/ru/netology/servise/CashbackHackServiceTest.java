ff1848255c81732d29b6c6b672747ae88e8c6cd7
evgenijerm <erm.evgeniy18@gmail.com>
2020-06-08 10:37:31 +0300

    delete Amount1000

--- src/test/java/ru/netology/servise/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/servise/CashbackHackServiceTest.java
@@ -25,14 +25,6 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void Amount1000() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int expect = 1000;
-        int actual = service.remain(amount);
-        assertEquals(expect, actual);
-    }
-    @Test
     public void Expect0() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
